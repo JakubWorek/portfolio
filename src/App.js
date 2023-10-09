1cf244e18bbce3b828ef2ec8eff24f6297dbfc91
Jakub Worek <worekkuba@gmail.com>
2023-10-09 19:20:25 +0200

    skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Skills />
       <SocialLinks />
     </div>
   );
---
+++ src/components/Skills.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const Skills = () => {
+  const technologies = [
+    {
+      id: 1,
+      name: 'JavaScript',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+      style: 'shadow-yellow-500',
+    },
+    {
+      id: 2,
+      name: 'TypeScript',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+      style: 'shadow-blue-800',
+    },
+    {
+      id: 3,
+      name: 'React.js',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+      style: 'shadow-blue-500',
+    },
+    {
+      id: 4,
+      name: 'Next.js',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg',
+      style: 'shadow-gray-800',
+    },
+    {
+      id: 5,
+      name: 'Express.js',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg',
+      style: 'shadow-gray-600'
+    },
+    {
+      id: 6,
+      name: 'MongoDB',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
+      style: 'shadow-green-600',
+    },
+    {
+      id: 7,
+      name: 'Node.js',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+      style: 'shadow-green-500',
+    },
+    {
+      id: 8,
+      name: 'Tailwind CSS',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+      style: 'shadow-sky-400',
+    },
+    {
+      id: 9,
+      name: 'Git',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+      style: 'shadow-orange-700',
+    }
+  ];
+
+  return (
+    <div name='skills' className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center h-full">
+        <h1 className="text-4xl font-bold inline border-b border-gray-500 text-emerald-200"> Technologies I worked with </h1>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {technologies.map(({ id, name, src, style }) => (
+            <div key={id} className={`shadow-md hover:scale-110 duration-500 py-2 rounded-lg ${style}`}>
+              <img src={src} alt={name} className='w-20 mx-auto' />
+              <p className='mt-4'>
+                {name}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
