e3b30f03bf0e315e5432448b05219c3e50819789
Jakub Worek <worekkuba@gmail.com>
2023-10-09 20:27:47 +0200

    skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -65,7 +65,7 @@ const Skills = () => {
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {technologies.map(({ id, name, src, style }) => (
-            <div key={id} className={`shadow-md hover:scale-110 duration-500 py-2 rounded-lg ${style}`}>
+            <div key={id} className={`bg-gray-500 shadow-md hover:scale-110 duration-500 py-2 rounded-lg ${style}`}>
               <img src={src} alt={name} className='w-20 mx-auto' />
               <p className='mt-4'>
                 {name}
